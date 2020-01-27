2f07895ca82042034a710b38b3e1ae8b63b8b023
ansivgit <ansiv@yandex.ru>
2020-01-27 19:37:53 +0300

    linters added

--- source/blocks/dropdown/dropdown.js
+++ source/blocks/dropdown/dropdown.js
@@ -1,14 +1,14 @@
-//import '../templates/common-template.scss';
-//import 'dropdown.scss'
+// import '../templates/common-template.scss';
+// import 'dropdown.scss'
 
-//console.log('Hello drop.js!')
+// console.log('Hello drop.js!')
 
-let expandField = document.querySelector('.input__field--expanded');
+const expandField = document.querySelector('.input__field--expanded');
 
 expandField.addEventListener('click', function () {
   console.log('Клик по кнопке');
   expandField.classList.toggle('input__field--expanded');
 
-  let expand = document.querySelector('.expand');
+  const expand = document.querySelector('.expand');
   expand.classList.toggle('visually-hidden');
 });
